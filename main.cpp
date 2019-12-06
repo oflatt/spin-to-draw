b1b8cb9e91e868dcfcbee4f3cfedd36b7dbd3d0b
oliver <oflatt@gmail.com>
2019-12-06 10:40:06 -0700

    add draw your own shape

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@
 
 unsigned int g_windowWidth = 1000;
 unsigned int g_windowHeight = 800;
+float modelWidth = 1.0;
+float modelHeight = ((float) g_windowHeight) / ((float) g_windowWidth);
+
 char* g_windowName = "HW3-3D-Basics";
 
 GLFWwindow* g_window;
@@ -31,22 +34,78 @@ bool enableDolly = false;
 bool showCheckerboard = false;
 
 // Dolly zoom options 
-float fov = M_PI / 4.f;
-float initialDistance = 4.5f;
-float distance = 4.5f;
+float fov = M_PI / 2.f;
+
+// line up distance so that it matches the screen
+
+float distance = (1.0 / tan(fov/2)) * 0.25;
 
 // per second
 float rotation_rate = 0.05f;
 float fovMoveSpeed = 0.1f;
+
+// these define the coordinate system
+// the model is stored centered around 0, 0 and stretching from -0.5 to 0.5 along both x and y axis
+std::complex<float> model_to_screen(std::complex<float> modelCoordinate) {
+  return std::complex<float>(modelCoordinate.real(),
+			     modelCoordinate.imag());
+}
+
+std::complex<float> mouse_to_model(double mouseX, double mouseY) {
+  return std::complex<float>((mouseX - g_windowWidth/2) / g_windowWidth * modelWidth,
+			     -((mouseY - g_windowHeight/2) / g_windowHeight)*modelHeight); // y axis gets flipped and center on zero
+}
+
 
 struct Rotating {
   std::complex<float> coefficient;
   int n;
 };
 
+
+struct State {
+  bool mousePressed;
+  std::vector<std::complex<float>> samples;
+  std::vector<Rotating> rotatings;
+
+  State() {
+    mousePressed = false;
+  }
+};
+
+State state;
+
+
 bool compareRotating(Rotating& r1, Rotating& r2) {
   return std::abs(r1.coefficient) > std::abs(r2.coefficient);
 }
+
+
+const std::complex<float> complex_i(0, 1);
+int granularity = 11;
+int start = -(granularity / 2);
+
+std::vector<Rotating> make_rotating(std::vector<std::complex<float>>& samples) {
+  std::vector<Rotating> rotatings;
+  for(int i = 0; i < granularity; i++) {
+    int n = i + start;
+    std::complex<float> c(0, 0);
+    for(int samplei = 0; samplei < samples.size(); samplei++) {
+      float t = ((float) samplei) / ((float) samples.size());
+      c += samples[samplei] * std::exp(-n * M_PI * 2 * complex_i * t) / ((float) samples.size());
+    }
+    Rotating r;
+    r.coefficient = c;
+    r.n = n;
+    rotatings.push_back(r);
+  }
+
+  std::sort(rotatings.begin(), rotatings.end(), compareRotating);
+  
+  return rotatings;
+}
+
+
 
 // Auxiliary math functions
 float dotProduct(const float* a, const float* b)
@@ -74,96 +133,7 @@ void normalize(float* a)
 	a[2] /= len;
 }
 
-void meshIndicesToVec(float* dest, int index1, int index2) {
-	dest[0] = g_meshVertices[index2 * 3] - g_meshVertices[index1 * 3];
-	dest[1] = g_meshVertices[index2 * 3 + 1] - g_meshVertices[index1 * 3 + 1];
-	dest[2] = g_meshVertices[index2 * 3 + 2] - g_meshVertices[index1 * 3 + 2];
-}
 
-void computeNormals()
-{
-	g_meshNormals.resize(g_meshVertices.size());
-	for (int i = 0; i < g_meshNormals.size(); i++) {
-		g_meshNormals[i] = 0.0;
-	}
-
-
-	for (int v = 0; v < g_meshIndices.size() / 3; v++) {
-		unsigned int index1 = g_meshIndices[v * 3];
-		unsigned int index2 = g_meshIndices[v * 3 + 1];
-		unsigned int index3 = g_meshIndices[v * 3 + 2];
-
-
-		float vec1[3];
-		float vec2[3];
-		meshIndicesToVec(vec1, index1, index2);
-		meshIndicesToVec(vec2, index1, index3);
-
-		float normal[3];
-		crossProduct(vec1, vec2, normal);
-
-		g_meshNormals[index1 * 3] += normal[0];
-		g_meshNormals[index1 * 3 + 1] += normal[1];
-		g_meshNormals[index1 * 3 + 2] += normal[2];
-
-		g_meshNormals[index2 * 3] += normal[0];
-		g_meshNormals[index2 * 3 + 1] += normal[1];
-		g_meshNormals[index2 * 3 + 2] += normal[2];
-
-		g_meshNormals[index3 * 3] += normal[0];
-		g_meshNormals[index3 * 3 + 1] += normal[1];
-		g_meshNormals[index3 * 3 + 2] += normal[2];
-
-	}
-
-	float* vecData = g_meshNormals.data();
-
-	for (int v = 0; v < g_meshNormals.size() / 3; v++) {
-		normalize(vecData + (v * 3));
-
-	}
-}
-
-void loadObj(std::string p_path)
-{
-	std::ifstream nfile;
-	nfile.open(p_path);
-	std::string s;
-
-	while (nfile >> s)
-	{
-		if (s.compare("v") == 0)
-		{
-			float x, y, z;
-			nfile >> x >> y >> z;
-			g_meshVertices.push_back(x);
-			g_meshVertices.push_back(y);
-			g_meshVertices.push_back(z);
-		}
-		else if (s.compare("f") == 0)
-		{
-			std::string sa, sb, sc;
-			unsigned int a, b, c;
-			nfile >> sa >> sb >> sc;
-
-			a = std::stoi(sa);
-			b = std::stoi(sb);
-			c = std::stoi(sc);
-
-			g_meshIndices.push_back(a - 1);
-			g_meshIndices.push_back(b - 1);
-			g_meshIndices.push_back(c - 1);
-		}
-		else
-		{
-			std::getline(nfile, s);
-		}
-	}
-
-	computeNormals();
-
-	std::cout << p_path << " loaded. Vertices: " << g_meshVertices.size() / 3 << " Triangles: " << g_meshIndices.size() / 3 << std::endl;
-}
 
 double getTime()
 {
@@ -181,22 +151,13 @@ void togglePerspective() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	float halfWidth = initialDistance * tan(fov / 2);
+	float halfWidth = distance * tan(fov / 2);
 
 	// Perspective Projection
 	if (enablePersp)
 	{
-		float currentFov = fov;
 
-		distance = initialDistance;
-		// Dolly zoom computation
-		if (enableDolly) {
-			currentFov = (fov - fmod((getTime() * M_PI * fovMoveSpeed), fov * 0.99));
-			distance = halfWidth / tan(currentFov / 2);
-		}
-
-
-		float fovInDegree = radianToDegree(currentFov);
+		float fovInDegree = radianToDegree(fov);
 		gluPerspective(fovInDegree, (GLfloat)g_windowWidth / (GLfloat)g_windowHeight, 1.0f, 40.f);
 	}
 	// Othogonal Projection
@@ -210,101 +171,118 @@ void togglePerspective() {
 	}
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+  if(button == GLFW_MOUSE_BUTTON_LEFT) 
+    {
+      // reset state on button press
+      if(action == GLFW_PRESS) {
+	state = State();
+      } else {
+	// calculate vectors on release
+	state.rotatings = make_rotating(state.samples);
+      }
+
+      state.mousePressed = (action == GLFW_PRESS);
+    }
+}
+
 void glfwKeyCallback(GLFWwindow* p_window, int p_key, int p_scancode, int p_action, int p_mods)
 {
-	if (p_key == GLFW_KEY_ESCAPE && p_action == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(g_window, GL_TRUE);
-	}
-	if (p_key == GLFW_KEY_P && p_action == GLFW_PRESS) {
+  
+  if (p_key == GLFW_KEY_ESCAPE && p_action == GLFW_PRESS)
+    {
+      glfwSetWindowShouldClose(g_window, GL_TRUE);
+    }
+  if (p_key == GLFW_KEY_P && p_action == GLFW_PRESS) {
 
-		// Perspective Projection
-		enablePersp = true;
-		togglePerspective();
-		std::cout << "Perspective activated\n";
+    // Perspective Projection
+    enablePersp = true;
+    togglePerspective();
+    std::cout << "Perspective activated\n";
 
-	}
-	if (p_key == GLFW_KEY_O && p_action == GLFW_PRESS) {
+  }
+  if (p_key == GLFW_KEY_O && p_action == GLFW_PRESS) {
 
-		// Orthographic Projection
-		enablePersp = false;
-		togglePerspective();
-		std::cout << "Orthogonal activated\n";
+    // Orthographic Projection
+    enablePersp = false;
+    togglePerspective();
+    std::cout << "Orthogonal activated\n";
 
-	}
-	if (p_key == GLFW_KEY_S && p_action == GLFW_PRESS) {
+  }
+  if (p_key == GLFW_KEY_S && p_action == GLFW_PRESS) {
 
-		// Toggle Spinning
-		if (!teapotSpin) {
-			std::cout << "Teapot spinning on\n";
-		}
-		else {
-			std::cout << "Teapot spinning off\n";
-		}
-		teapotSpin = !teapotSpin;
-	}
-	if (p_key == GLFW_KEY_D && p_action == GLFW_PRESS) {
+    // Toggle Spinning
+    if (!teapotSpin) {
+      std::cout << "Teapot spinning on\n";
+    }
+    else {
+      std::cout << "Teapot spinning off\n";
+    }
+    teapotSpin = !teapotSpin;
+  }
+  if (p_key == GLFW_KEY_D && p_action == GLFW_PRESS) {
 
-		// Toggle dolly zoom
-		if (!enableDolly)
-		{
-			std::cout << "Dolly zoom on\n";
-		}
-		else {
-			std::cout << "Dolly zoom off\n";
-		}
-		enableDolly = !enableDolly;
-	}
-	if (p_key == GLFW_KEY_C && p_action == GLFW_PRESS) {
+    // Toggle dolly zoom
+    if (!enableDolly)
+      {
+	std::cout << "Dolly zoom on\n";
+      }
+    else {
+      std::cout << "Dolly zoom off\n";
+    }
+    enableDolly = !enableDolly;
+  }
+  if (p_key == GLFW_KEY_C && p_action == GLFW_PRESS) {
 
-		// Show/hide Checkerboard
-		if (!showCheckerboard)
-		{
-			std::cout << "Show checkerboard\n";
-		}
-		else {
-			std::cout << "Hide checkerboard\n";
-		}
-		showCheckerboard = !showCheckerboard;
-	}
+    // Show/hide Checkerboard
+    if (!showCheckerboard)
+      {
+	std::cout << "Show checkerboard\n";
+      }
+    else {
+      std::cout << "Hide checkerboard\n";
+    }
+    showCheckerboard = !showCheckerboard;
+  }
 }
 
 void initWindow()
 {
-	// initialize GLFW
-	glfwSetErrorCallback(glfwErrorCallback);
-	if (!glfwInit())
-	{
-		std::cerr << "GLFW Error: Could not initialize GLFW library" << std::endl;
-		exit(1);
-	}
+  // initialize GLFW
+  glfwSetErrorCallback(glfwErrorCallback);
+  if (!glfwInit())
+    {
+      std::cerr << "GLFW Error: Could not initialize GLFW library" << std::endl;
+      exit(1);
+    }
 
-	g_window = glfwCreateWindow(g_windowWidth, g_windowHeight, g_windowName, NULL, NULL);
-	if (!g_window)
-	{
-		glfwTerminate();
-		std::cerr << "GLFW Error: Could not initialize window" << std::endl;
-		exit(1);
-	}
+  g_window = glfwCreateWindow(g_windowWidth, g_windowHeight, g_windowName, NULL, NULL);
+  if (!g_window)
+    {
+      glfwTerminate();
+      std::cerr << "GLFW Error: Could not initialize window" << std::endl;
+      exit(1);
+    }
 
-	// callbacks
-	glfwSetKeyCallback(g_window, glfwKeyCallback);
+  // callbacks
+  glfwSetKeyCallback(g_window, glfwKeyCallback);
+  glfwSetMouseButtonCallback(g_window, mouse_button_callback);
 
-	// Make the window's context current
-	glfwMakeContextCurrent(g_window);
+  // Make the window's context current
+  glfwMakeContextCurrent(g_window);
 
-	// turn on VSYNC
-	glfwSwapInterval(1);
+  // turn on VSYNC
+  glfwSwapInterval(1);
 }
 
 void initGL()
 {
-	glClearColor(1.f, 1.f, 1.f, 1.0f);
+  glClearColor(1.f, 1.f, 1.f, 1.0f);
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
-	glShadeModel(GL_SMOOTH);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_DEPTH_TEST);
+  glShadeModel(GL_SMOOTH);
 }
 
 void printHotKeys() {
@@ -360,40 +338,8 @@ void setModelViewMatrix()
 	glLoadMatrixf(g_modelViewMatrix);
 }
 
-const std::complex<float> complex_i(0, 1);
-int granularity = 11;
-int start = -(granularity / 2);
-
-std::vector<Rotating> make_rotating(std::vector<std::complex<float>>& samples) {
-  std::vector<Rotating> rotatings;
-  for(int i = 0; i < granularity; i++) {
-    int n = i + start;
-    std::complex<float> c(0, 0);
-    for(int samplei = 0; samplei < samples.size(); samplei++) {
-      float t = ((float) samplei) / ((float) samples.size());
-      c += samples[samplei] * std::exp(-n * M_PI * 2 * complex_i * t) / ((float) samples.size());
-    }
-    Rotating r;
-    r.coefficient = c;
-    r.n = n;
-    rotatings.push_back(r);
-  }
-
-  std::sort(rotatings.begin(), rotatings.end(), compareRotating);
-  
-  return rotatings;
-}
 
 void renderLines() {
-
-  
-  std::vector<std::complex<float>> samples = {std::complex<float>(-2.0, 2.0), std::complex<float>(0, 2.0),
-						std::complex<float>(2.0, 2.0),
-				std::complex<float>(2.0, 0.0), std::complex<float>(2.0, -2.0),
-				std::complex<float>(0.0, -2.0), std::complex<float>(-2.0, -2.0)};
-
-  std::vector<Rotating> rotatings = make_rotating(samples);
-  
   
 
   glDisable(GL_LIGHTING);
@@ -405,14 +351,43 @@ void renderLines() {
   float time = getTime() * 2 * M_PI * rotation_rate;
   std::complex<float> currentpos(0, 0);
 
-  for(Rotating r : rotatings) {
+  for(Rotating r : state.rotatings) {
     int n = r.n;
     std::complex<float> oldpos(currentpos);
     currentpos += r.coefficient * std::exp(n * 2 * M_PI * complex_i * time);
-    glVertex3f(oldpos.real(), oldpos.imag(), distance);
-    glVertex3f(currentpos.real(), currentpos.imag(), distance);
+
+    std::complex<float> oldposScreen = model_to_screen(oldpos);
+    std::complex<float> currentposScreen = model_to_screen(currentpos);
+    glVertex3f(oldposScreen.real(), oldposScreen.imag(), distance);
+    glVertex3f(currentposScreen.real(), currentposScreen.imag(), distance);
   }
 
+  glEnd();
+
+  glEnable(GL_LIGHTING);
+}
+
+
+void renderDrawing() {
+  glDisable(GL_LIGHTING);
+
+  glColor3f(0, 0, 0);
+  glLineWidth(3);
+  glBegin(GL_LINES);
+  
+
+  for(int i = 0; i < state.samples.size(); i++) {
+    int lastIndex = i - 1;
+    if(lastIndex < 0) {
+      lastIndex = state.samples.size() -1;
+    }
+    
+    std::complex<float> last = model_to_screen(state.samples[lastIndex]);
+    std::complex<float> current = model_to_screen(state.samples[i]);
+    glVertex3f(last.real(), last.imag(), distance);
+    glVertex3f(current.real(), current.imag(), distance);
+  }
+  
   glEnd();
 
   glEnable(GL_LIGHTING);
@@ -443,6 +418,7 @@ void drawCheckerBoard() {
 	}
 	glEnd();
 }
+
 void renderCheckerBoard() {
 
 	/*
@@ -473,10 +449,26 @@ void render()
 {
 	togglePerspective();
 	setModelViewMatrix();
-	renderLines();
+
+	renderDrawing();
+	if (!state.mousePressed) {
+	  renderLines();
+	}
+	
+
 	if (showCheckerboard)
 		renderCheckerBoard();
 }
+
+void onTick(GLFWwindow* window) {
+  if (state.mousePressed) {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    
+    state.samples.push_back(mouse_to_model(xpos, ypos));
+  }
+}
+
 
 void renderLoop()
 {
@@ -492,6 +484,8 @@ void renderLoop()
 
 		// Poll for and process events
 		glfwPollEvents();
+		
+		onTick(g_window);
 	}
 }
 
@@ -499,7 +493,6 @@ int main()
 {
 	initWindow();
 	initGL();
-	loadObj("data/teapot.obj");
 	printHotKeys();
 	renderLoop();
 }
